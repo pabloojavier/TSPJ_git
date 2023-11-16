2542a78d25d6508960b513c7690ead569c9b2dbf
pablogutierrezbci <pablo.gutierrez@bci.cl>
2023-11-16 10:52:08 -0300

    wilcoxon changes

--- wilcoxon.py
+++ wilcoxon.py
@@ -2,14 +2,17 @@ from numpy.random import seed
 from scipy.stats import wilcoxon
 import pandas as pd
 import numpy as np
+import warnings
+# Set the desired action for the warning
+warnings.filterwarnings("ignore")
 
 # seed the random number generator
 #name = sys.argv[1] # data file
 seed(1)
-instancias = ["large","medium","small","tsplib"]
+instancias = ["small","tsplib"]
 for k in instancias:
 #for i in range(7,8):
-    name = f"output/gap_milp_{k}.txt"
+    name = f"output/gap_dl_short_{k}.txt"
     data = [[] for j in range(2)]
     f = open(name, 'r')
     for line in f:
@@ -20,5 +23,5 @@ for k in instancias:
 
     alpha = 0.05
     stat, p = wilcoxon(data[0], data[1])#, zero_method='pratt', correction=True)
-    print(f"MGA {k}: p={p}")
+    print(f"dl_short {k}: p={p}")
 print()
